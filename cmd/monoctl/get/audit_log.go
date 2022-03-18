0453942c8896e4ee653b4a927185384297d206aa
HaniAlshikh <Alshikh.hani@gmail.com>
2022-03-18 13:56:23 +0100

    changed to standered (ISO8601) layout for date input)
    Signed-off-by: HaniAlshikh <Alshikh.hani@gmail.com>

--- cmd/monoctl/get/audit_log.go
+++ cmd/monoctl/get/audit_log.go
@@ -27,15 +27,15 @@ import (
 )
 
 func NewGetAuditLogCmd() *cobra.Command {
+	const dateLayoutISO8601 = "2006-01-02" // don't change. This corresponds to YYYY-MM-DD
 	var (
 		from string
 		to string
 
-		layout = "02.01.2006" // don't change. This corresponds to DD.MM.YYYY
 		now = time.Now()
 		firstOfMonth = time.Date(now.Year(), now.Month(), 1, 0, 0, 0, 0, time.UTC)
 		lastOfMonth = firstOfMonth.AddDate(0, 1, -1)
-		dateInputErr = func(input string) error { return fmt.Errorf("%s is invalid.\nPlease make sure to use the correct date layout. Example: %s", input, now.Format(layout))}
+		dateInputErr = func(input string) error { return fmt.Errorf("%s is invalid.\nPlease make sure to use the correct date layout. Example: %s", input, now.Format(dateLayoutISO8601))}
 	)
 
 	cmd := &cobra.Command{
@@ -44,14 +44,14 @@ func NewGetAuditLogCmd() *cobra.Command {
 		Short:   "Get audit log",
 		Long:    `Get audit log based on a date range. If no date range is specified the audit log of the current month will be returned. Please note that both input and output time is in UTC and should be considered when e.g. specifying the date range`,
 		RunE: func(cmd *cobra.Command, args []string) error {
-			minTime, err := time.Parse(layout, from)
+			minTime, err := time.Parse(dateLayoutISO8601, from)
 			if err != nil {
 				if len(from) != 0 { // if not specified first day of current month is used
 					return dateInputErr(from)
 				}
 				minTime = firstOfMonth
 			}
-			maxTime, err := time.Parse(layout, to)
+			maxTime, err := time.Parse(dateLayoutISO8601, to)
 			if err != nil {
 				if len(to) != 0 { // if not specified last day of the current month is used
 					return dateInputErr(to)
@@ -68,10 +68,10 @@ func NewGetAuditLogCmd() *cobra.Command {
 	}
 
 	cmdFlags := cmd.Flags()
-	cmdFlags.StringVarP(&from, "from", "f", firstOfMonth.Format(layout),
-		fmt.Sprintf("Specifys the starting point of the date range. If not specified the first day of the current month is used. Accepted layout: %s", now.Format(layout)))
-	cmdFlags.StringVarP(&to, "to", "t", lastOfMonth.Format(layout),
-		fmt.Sprintf("Specifys the ending point of the date range. If not specified the last day of the current month is used. Accepted layout: %s", now.Format(layout)))
+	cmdFlags.StringVarP(&from, "from", "f", firstOfMonth.Format(dateLayoutISO8601),
+		fmt.Sprintf("Specifys the starting point of the date range. If not specified the first day of the current month is used. Accepted layout: %s", now.Format(dateLayoutISO8601)))
+	cmdFlags.StringVarP(&to, "to", "t", lastOfMonth.Format(dateLayoutISO8601),
+		fmt.Sprintf("Specifys the ending point of the date range. If not specified the last day of the current month is used. Accepted layout: %s", now.Format(dateLayoutISO8601)))
 
 	return cmd
 }
