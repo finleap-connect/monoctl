f210253e9cb12bc4981a7029a74c61bc6c69cee2
Jan Steffen <jan.steffen@finleap.com>
2022-03-25 12:22:20 +0100

    Bump to 1.18, add arm build for darwin, fix dep automaxprocs (#51)
    * Bump to 1.18, add arm build for darwin, fix dep automaxprocs
    * Fix dependencies

--- go.mk
+++ go.mk
@@ -21,6 +21,7 @@ PROTOC     	   ?= protoc
 
 CMD_MONOCTL_LINUX = $(BUILD_PATH)/monoctl-linux-amd64
 CMD_MONOCTL_OSX = $(BUILD_PATH)/monoctl-osx-amd64
+CMD_MONOCTL_OSX_ARM = $(BUILD_PATH)/monoctl-osx-arm64
 CMD_MONOCTL_WIN = $(BUILD_PATH)/monoctl-win-amd64
 CMD_MONOCTL_SRC = cmd/monoctl/*.go
 
@@ -89,12 +90,21 @@ $(CMD_MONOCTL_LINUX): ## build monoctl for linux
 $(CMD_MONOCTL_OSX): ## build monoctl for osx
 	CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 $(GO) build -a $(BUILDFLAGS) -ldflags "$(LDFLAGS) -X=$(GO_MODULE_MONOSKOPE)/pkg/logger.logMode=noop" -o $(CMD_MONOCTL_OSX) $(CMD_MONOCTL_SRC)
 
+$(CMD_MONOCTL_OSX_ARM): ## build monoctl for osx arm
+	CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 $(GO) build -a $(BUILDFLAGS) -ldflags "$(LDFLAGS) -X=$(GO_MODULE_MONOSKOPE)/pkg/logger.logMode=noop" -o $(CMD_MONOCTL_OSX_ARM) $(CMD_MONOCTL_SRC)
+
 $(CMD_MONOCTL_WIN): ## build monoctl for windows
 	CGO_ENABLED=0 GOOS=windows GOARCH=amd64 $(GO) build -a $(BUILDFLAGS) -ldflags "$(LDFLAGS) -X=$(GO_MODULE_MONOSKOPE)/pkg/logger.logMode=noop" -o $(CMD_MONOCTL_WIN) $(CMD_MONOCTL_SRC)
 
 build-monoctl-linux: $(CMD_MONOCTL_LINUX) ## build monoctl for linux
+	@chmod a+x $(CMD_MONOCTL_LINUX)
+build-monoctl-osx: $(CMD_MONOCTL_OSX) $(CMD_MONOCTL_OSX_ARM) ## build monoctl for osx
+	@chmod a+x $(CMD_MONOCTL_OSX)
+	@chmod a+x $(CMD_MONOCTL_OSX_ARM)
+build-monoctl-win: $(CMD_MONOCTL_WIN)  ## build monoctl for windows
+	@chmod a+x $(CMD_MONOCTL_WIN)
 
-build-monoctl-all: $(CMD_MONOCTL_LINUX) $(CMD_MONOCTL_OSX) $(CMD_MONOCTL_WIN) ## build monoctl for linux, osx and windows
+build-monoctl-all: build-monoctl-linux build-monoctl-osx build-monoctl-win ## build monoctl for linux, osx and windows
 
 rebuild-mocks: ## rebuild go mocks
 	$(MOCKGEN) -package eventsourcing -destination test/mock/eventsourcing/command_handler_client.go github.com/finleap-connect/monoskope/pkg/api/eventsourcing CommandHandlerClient
--- go.mod
+++ go.mod
@@ -1,11 +1,11 @@
 module github.com/finleap-connect/monoctl
 
-go 1.17
+go 1.18
 
 require (
 	github.com/briandowns/spinner v1.18.1
 	github.com/cenkalti/backoff v2.2.1+incompatible
-	github.com/finleap-connect/monoskope v0.3.5-rc0
+	github.com/finleap-connect/monoskope v0.3.5
 	github.com/golang/mock v1.6.0
 	github.com/google/uuid v1.3.0
 	github.com/kubism/testutil v0.1.0-alpha.2
@@ -84,7 +84,6 @@ require (
 	github.com/xeipuuv/gojsonreference v0.0.0-20180127040603-bd5ef7bd5415 // indirect
 	github.com/xeipuuv/gojsonschema v1.2.0 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
-	go.uber.org/automaxprocs v1.4.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
 	go.uber.org/zap v1.17.0 // indirect
 	golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd // indirect
